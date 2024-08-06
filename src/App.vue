041631deb95675dea6e89fc30d930f30bbdf91a7
Mohamed Soumare <mohamedsoumare17763@gmail.com>
2024-08-06 16:10:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <HelloWord />
+         
+        <!-- <template v-slot:name>
+              {{ user.name }}    
+        </template>
+
+        <template v-slot:age>
+            {{ user.age}}
+        </template> -->
+        <div :class="{ 'text-green':user.age>23}"> {{ user.name }}</div>
+    
+    <!-- <HelloWord :user="user" /> -->
+    
+    <!-- <h2>{{ subTitle }}</h2> -->
+    <!-- <input type="text" v-model="user.name" placeholder="Enter name">
+    <input type="number" v-model="user.age" placeholder="Enter age"> -->
+
+  </template>
+  
+  <script setup>
+  
+  import HelloWord from './components/HelloWord.vue';
+  import { reactive } from 'vue';
+  
+  const user = reactive({
+    name: 'NordCoders',
+    age: 30,
+  });
+  
+  const display = () =>{
+    console.log(user.name,user.age);
+  }
+  </script>
+  
+  <style scoped>
+    .text-red {
+        
+        color: indianred;
+    }
+    .text-green {
+        color: green;
+    }
+  </style>
+  
---
+++ src/components/HelloWord.vue
@@ -0,0 +1,19 @@
+<template>
+   <h1>Hello World</h1>
+   <!-- <p>How are you?</p> -->
+   <!-- <p>{{ props.user.name }} is {{ props.user.age }} years old.</p> -->
+   <!-- <slot name="name">
+       
+   </slot>
+
+   <slot name="age">
+       
+   </slot> -->
+   
+ </template>
+ 
+ <script setup>
+   
+ </script>
+ 
+ 
